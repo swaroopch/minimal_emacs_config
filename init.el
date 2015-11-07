7961d2141668d83c1b6137c09e798224374143f8
Swaroop C H <swaroop@automatic.com>
2015-11-06 22:13:41 -0800

    Changes for Automatic internal lunch & learn

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 (tool-bar-mode -1)
 
 ;; Font
-(set-frame-font "Fira Mono-14" nil t)
+(set-frame-font "Monaco-14" nil t)
 
 ;; Package management
 (require 'package)
@@ -40,7 +40,6 @@
 
 (defvar my-packages
   '(helm
-    org
     solarized-theme))
 
 (require-packages my-packages)
@@ -69,51 +68,48 @@
 (define-key helm-map (kbd "C-z") 'helm-select-action)
 
 ;;; Org Mode ;;;
-(global-set-key (kbd "C-c c") 'org-capture)
-
 (require 'org)
-(require 'org-id)
 (require 'org-habit)
-(require 'org-attach)
 (add-to-list 'org-modules 'org-habit)
 
 ;; Default directory
-(setq org-directory "~/Box/")
+(setq org-directory "~/notes/")
 
-;; Disable flycheck in Org Mode
-;; (add-hook 'org-mode-hook (lambda ()
-;;                            (flycheck-mode -1)))
+;; Indented mode
+(setq org-startup-indented t)
 
 ;; Display inline images
 (setq org-startup-with-inline-images t)
+
+;; Line-breaking
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
 
 ;; Capture
 ;; http://orgmode.org/manual/Setting-up-capture.html
 (setq org-default-notes-file (concat org-directory "inbox.org"))
 
+(global-set-key (kbd "C-c c") 'org-capture)
+
 ;; ~<Leader> o~ is the open space for user-defined shortcuts
 ;; http://orgmode.org/org.html#Capture-templates
-;; (global-set-key (kbd "C-c o c") 'org-capture)
 (setq org-capture-templates
       '(("i" "Inbox" entry (file+headline (concat org-directory "inbox.org")
                                           "Inbox")
          "* TODO %?\n  %U\n  %i\n")
         ("d" "Diary" entry (file+datetree (concat org-directory "diary.org"))
-         "* %?\n  %i\n")
-        ("f" "Food" entry (file+datetree (concat org-directory "food.org"))
          "* %?\n  %i\n")))
 
 ;; Link abbreviations
 ;; http://orgmode.org/manual/Link-abbreviations.html
 (setq org-link-abbrev-alist
-      '(("task" . "https://phabricator.automatic.co/%s")
-        ("diff" . "https://phabricator.automatic.co/%s")))
+      '(("task" . "https://phabricator.automatic.co/T%s")
+        ("diff" . "https://phabricator.automatic.co/D%s")))
 
 ;; Agenda
 (setq org-agenda-files (list (concat org-directory "automatic.org")
-                             (concat org-directory "control.org")
+			     (concat org-directory "personal.org")
                              (concat org-directory "inbox.org")))
-(setq org-agenda-span 2)
+(setq org-agenda-span 3)
 (setq org-agenda-start-on-weekday nil)
 
 ;; http://orgmode.org/worg/org-faq.html#agenda-wrong-time-of-day
@@ -141,17 +137,6 @@
 
 ;; Effort estimation
 ;; http://orgmode.org/worg/doc.html#org-effort-durations
-;; (defvar swa/org-hour 60)
-;; (defvar swa/org-day (* 4 swa/org-hour))
-;; (defvar swa/org-week (* 5 swa/org-day))
-;; (defvar swa/org-month (* 4 swa/org-week))
-;; (defvar swa/org-year (* 10 swa/org-month))
-;; (setq org-effort-durations
-;;       '(("h" . swa/org-hour)
-;;         ("d" . swa/org-day)
-;;         ("w" . swa/org-week)
-;;         ("m" . swa/org-month)
-;;         ("y" . swa/org-year)))
 (setq org-effort-durations
       '(("h" . 60)
         ("d" . 240)
@@ -164,7 +149,7 @@
 (setq org-src-fontify-natively t)
 
 ;; Key bindings
-(org-defkey org-mode-map "\C-c\C-j" 'helm-org-in-buffer-headings)
+(org-defkey org-mode-map (kbd "C-c C-j") 'helm-org-in-buffer-headings)
 
 ;; Shortcuts to commonly-used Org files
 (global-set-key (kbd "s-1") (lambda ()
@@ -173,26 +158,12 @@
 
 (global-set-key (kbd "s-2") (lambda ()
                               (interactive)
-                              (find-file (concat org-directory "control.org"))))
-
-(global-set-key (kbd "s-3") (lambda ()
-                              (interactive)
                               ;; ~C-c a a~
                               (org-agenda-list)))
 
-(global-set-key (kbd "s-4") (lambda ()
+(global-set-key (kbd "s-3") (lambda ()
                               (interactive)
                               (find-file (concat org-directory "inbox.org"))))
-
-(global-set-key (kbd "s-5") (lambda ()
-                              (interactive)
-                              (find-file (concat org-directory "reference.org"))))
-
-;; Line-breaking
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
-
-;; Spell check
-(add-hook 'org-mode-hook '(lambda () (flyspell-mode -1)))
 
 ;; Refile settings
 ;; Taken from https://github.com/vedang/emacs-config/blob/master/configuration/org-mode-config.el
@@ -202,5 +173,12 @@
       org-refile-use-outline-path 'file
       org-outline-path-complete-in-steps nil
       org-refile-allow-creating-parent-nodes 'confirm)
+
+;; python for org babel
+;; http://emacs-fu.blogspot.com/2011/02/executable-source-code-blocks-with-org.html
+(setq org-confirm-babel-evaluate nil)
+
+(org-babel-do-load-languages 'org-babel-load-languages '( (python . t)
+							  (emacs-lisp . t) ))
 
 ;;; init.el ends here
